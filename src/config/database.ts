2e4070679f19e1b9c5e54988172c722796329742
Harathi <admin@admins-MacBook-Pro-2.local>
2026-02-12 23:50:05 +0530

    Add database configuration using Sequelize

---
+++ src/config/database.ts
@@ -0,0 +1,23 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const sequelize = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT as any,
+});
+
+export const connectDB = async() =>{
+  try {
+    await sequelize.authenticate();
+    console.log("Database connected successfully");
+  } catch (error) {
+    throw new Error("Unable to connect to database");
+  }
+}
+
+export default sequelize;
